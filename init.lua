3f70d42a235d53dfcff77c9a23f8ba38d5a2e52a
BrakeZap <kjawesome56@gmail.com>
2024-12-04 23:53:14 -0800

    Added key map for opening the file explorer

--- init.lua
+++ init.lua
@@ -190,6 +190,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Setup keymap for opening Mini File Explorer
+vim.keymap.set('n', '<leader>e', function()
+  MiniFiles.open()
+end, { desc = 'Open File Explorer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
